d1b0d109c24334f041630605f60e7ece15ced4c7
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2025-03-06 16:26:53 +0400

    feat: add conditional rendering on nav based on session token

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,23 @@
+import getAuthUsers from "@/lib/getAuthUser";
 import NavLink from "./NavLink";
 
-const Navigation = () => {
+const Navigation = async () => {
+  const authUser = await getAuthUsers();
+
   return (
     <nav>
       <NavLink href="/" label="Home" />
       <div>
-        <NavLink href="/register" label="Register" />
-
-        <NavLink href="/dashboard" label="Dashboard" />
-        <NavLink href="/login" label="Login" />
+        {authUser ? (
+          <div className="flex items-center">
+            <NavLink href="/dashboard" label="Dashboard" />
+          </div>
+        ) : (
+          <div>
+            <NavLink href="/register" label="Register" />
+            <NavLink href="/login" label="Login" />
+          </div>
+        )}
       </div>
     </nav>
   );
